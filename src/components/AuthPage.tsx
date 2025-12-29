12241fb373efea6b9ee6532bd5704afb381107c8
Shubham Kumar Jha <94341058+shubhamjhammmut@users.noreply.github.com>
2025-12-29 11:47:21 +0530

    Refactor logo import and usage in AuthPage

--- src/components/AuthPage.tsx
+++ src/components/AuthPage.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { Mail, Lock, User, ShieldCheck } from "lucide-react";
-import logo from "../assets/logo.png";
-<img src={logo} alt="NagarNetra Logo" className="w-32 h-auto" />
+import logoImage from "../assets/logo.png";
 
 
 import { loginUser, signupUser } from "../firebase/auth";
